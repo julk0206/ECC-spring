28ae54b3e91f26f4f2f0c5138071b57aa62c3501
수연 <julk0206@gmail.com>
2025-11-24 01:32:37 +0900

    Week 11

---
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,26 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor //기본 생성자 추가 어노테이션
+@ToString
+@Getter
+@Entity //엔티티 선언
+public class Article {
+    @Id // 엔티티의 대표값 지정
+    @GeneratedValue // 자동 생성 기능 추가 (숫자가 자동으로 매겨짐)
+    private Long id;
+    @Column // title 필드 선언, DB 테이블의 title 열과 연결됨
+    private String title;
+    @Column // content 필드 선언, DB 테이블의 content 열과 연결됨
+    private String content;
+
+}
